740c262933aa09794494259483fc473c7d064bf6
Smushed <smushedcode@gmail.com>
2021-09-03 18:43:50 -0500

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MazeFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
